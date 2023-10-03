f423f20f43338664e3744c141233dfef712423d0
Caleb L'Italien <caleblitalien@proton.me>
2023-10-03 13:06:51 -0400

    chap 11 done

---
+++ adder/target/debug/deps/adder-471d1202a6031c65.d
@@ -0,0 +1,5 @@
+/Users/caleblitalien/rustProjects/adder/target/debug/deps/adder-471d1202a6031c65: src/lib.rs
+
+/Users/caleblitalien/rustProjects/adder/target/debug/deps/adder-471d1202a6031c65.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-f18b53d6198a9edf.d
@@ -0,0 +1,7 @@
+/Users/caleblitalien/rustProjects/adder/target/debug/deps/adder-f18b53d6198a9edf.rmeta: src/lib.rs
+
+/Users/caleblitalien/rustProjects/adder/target/debug/deps/libadder-f18b53d6198a9edf.rlib: src/lib.rs
+
+/Users/caleblitalien/rustProjects/adder/target/debug/deps/adder-f18b53d6198a9edf.d: src/lib.rs
+
+src/lib.rs:
